d8cee212173276deed5a6ccfef239edb9af3acac
mauro2 <mauro2@668c4f68-67d6-4dea-8ad2-d905167ba806>
2012-03-20 15:09:51 +0000

    Modulo para modificar exámenes

--- lib/model/Ejercicio_corregido.php
+++ lib/model/Ejercicio_corregido.php
@@ -9,4 +9,15 @@
  */ 
 class Ejercicio_corregido extends BaseEjercicio_corregido
 {
+  public static  function getEjerciciosCursoEstado($id_usuario, $id_ejercicio, $corregidos=false)
+  {
+    $c = new Criteria();
+    $c->add(Ejercicio_resueltoPeer::ID_AUTOR, $id_usuario);
+    $c->add(Ejercicio_resueltoPeer::ID_EJERCICIO, $id_ejercicio);
+
+    if ($corregidos) {
+    	$c->add(Ejercicio_resueltoPeer::ID_CORRECTOR, NULL, Criteria::NOT_EQUAL);
+    }
+    return Ejercicio_resueltoPeer::DoCount($c);
+  }
 }
