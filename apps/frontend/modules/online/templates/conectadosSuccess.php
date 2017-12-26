520dc68104740894c0f2f5faa78d5bed6d69fb48
mauro <mauro@icox.com.ar>
2017-12-26 18:42:21 +0000

    se desconfigura y el gráfico que sale no da ninguna información

--- apps/frontend/modules/online/templates/conectadosSuccess.php
+++ apps/frontend/modules/online/templates/conectadosSuccess.php
@@ -11,23 +11,23 @@
 
   <div class="tit_box_menu"><h2 class="titbox">Usuarios conectados:</h2></div>
     <ul class="listamenu">
-        <?foreach ($onlines as $users_online) :?>
+        <?php foreach ($onlines as $users_online) :?>
 
-          <?foreach ($users_online as $usuario) :?>
+          <?php foreach ($users_online as $usuario) :?>
             <li class="infopersonal">
-                  <? if ('profesor' == $usuario->getRol()->getNombre() ) : ?>
+                  <?php if ('profesor' == $usuario->getRol()->getNombre() ) : ?>
                      <div style='text-align:left;padding:5px;padding-left:40px;color:red'>
-                  <? else : ?>
+                  <?php else : ?>
                      <div style='text-align:left;padding:5px;padding-left:40px;'>
-                  <? endif; ?>
-                     <? echo $usuario->getUsuario()->getNombreusuario()?>
+                  <?php endif; ?>
+                     <?php echo $usuario->getUsuario()->getNombreusuario()?>
                 </div>
             </li>
-          <?endforeach;?>
-        <?endforeach;?>
+          <?php endforeach;?>
+        <?php endforeach;?>
         <li class='conectados_c'>
           <div style='text-align:left;padding:5px;padding-left:40px;'>
-             <?echo $sf_user->getNumUsuariosConectados()?> Usuarios online
+             <?php echo $sf_user->getNumUsuariosConectados()?> Usuarios online
           </div>
         </li>
    </ul>
