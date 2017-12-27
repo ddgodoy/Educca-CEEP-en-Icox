98fc4bdaccf4403d2b3a9a7511e8e3b58326f84e
mauro@icox.com.ar <mauro@icox.com.ar@668c4f68-67d6-4dea-8ad2-d905167ba806>
2017-12-27 18:41:30 +0000

    update nota

--- trunk/apps/frontend/modules/admin/templates/fichaModuloSuccess.php
+++ trunk/apps/frontend/modules/admin/templates/fichaModuloSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('informacion'); ?>
-<? if (isset($info)) :  ?>
+<?php if (isset($info)) :  ?>
   <?php include_component('paquete', 'fichaModulo',array('idmodulo' => $idmodulo,'info' => $info)) ?>
-<? else : ?>
+<?php else : ?>
   <?php include_component('paquete', 'fichaModulo',array('idmodulo' => $idmodulo)) ?>
-<? endif; ?>
+<?php endif; ?>
 
