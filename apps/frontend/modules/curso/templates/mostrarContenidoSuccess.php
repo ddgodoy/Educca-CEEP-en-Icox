3c36eb4278d0b6919b1c7389ab74fdb0cb8ea951
mauro <mauro@icox.com.ar>
2018-01-11 16:38:07 +0000

    update libro

--- apps/frontend/modules/curso/templates/mostrarContenidoSuccess.php
+++ apps/frontend/modules/curso/templates/mostrarContenidoSuccess.php
@@ -11,5 +11,5 @@
 <iframe id="frame_scorm" src="<?php echo $ruta ?>" width="<?php echo $width ?>" height="<?php echo $height ?>" border="0">
 </iframe>
 <div style="position: fixed; margin: 0px auto; width: 100%; background-color: blue; bottom: 0px; height: 10%;">
-    <button onclick="TemaFinish()" style="cursor: pointer; padding: 5px;">Finalizar Tema</button>
+    <button onclick="TemaFinish()" style="cursor: pointer; padding: 5px; margin-top: 15px;">Finalizar Tema</button>
 </div>
