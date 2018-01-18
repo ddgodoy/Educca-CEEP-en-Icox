15d216375ee1eba1863339e0b97207658dfdf5fc
mauro <mauro@icox.com.ar>
2018-01-18 18:32:11 +0000

    update imagen curso

--- apps/frontend/modules/admin/templates/imagenCursoSuccess.php
+++ apps/frontend/modules/admin/templates/imagenCursoSuccess.php
@@ -1,18 +1,18 @@
 <?php use_helper('SexyButton') ?>
 <div id="divadmin">
-  <div class="tit_box_mensajes"><h2 class="titbox">Imagen del curso: <?echo $curso->getNombre()?></h2></div>
+  <div class="tit_box_mensajes"><h2 class="titbox">Imagen del curso: <?phpecho $curso->getNombre()?></h2></div>
   <div class="cont_box_correo">
 
     
 
-  <?if (!isset($mostrarForm)) : ?>
+  <?php if(!isset($mostrarForm)) : ?>
         <table>
           <tr>
-            <td><?if (file_exists(sfConfig::get('sf_web_dir').DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'img_cursos'.DIRECTORY_SEPARATOR.'portada_'.$curso->getId().'.jpg')) : ?>
-                    <?echo image_tag('img_cursos/portada_'.$curso->getId().'.jpg?rand='.rand(1,9999),'Alt=imagen')?>
-                <? else :?>
-                    <?echo image_tag('img_cursos/noimg.gif','Alt=imagen')?>
-                <? endif; ?>
+            <td><?php if(file_exists(sfConfig::get('sf_web_dir').DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'img_cursos'.DIRECTORY_SEPARATOR.'portada_'.$curso->getId().'.jpg')) : ?>
+                    <?php echo image_tag('img_cursos/portada_'.$curso->getId().'.jpg?rand='.rand(1,9999),'Alt=imagen')?>
+                <?php else :?>
+                    <?php echo image_tag('img_cursos/noimg.gif','Alt=imagen')?>
+                <?php endif; ?>
             </td>
             <td>
                   <?php echo form_tag('admin/imagenCurso', 'multipart=true') ?>
@@ -28,9 +28,9 @@
                           <td> <table>
                                  <tr>
                                    <td><?php echo sexy_submit_tag('Insertar'); ?></td>
-                                       <?if (file_exists(sfConfig::get('sf_web_dir').DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'img_cursos'.DIRECTORY_SEPARATOR.'portada_'.$curso->getId().'.jpg')) : ?>
+                                       <?php if (file_exists(sfConfig::get('sf_web_dir').DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'img_cursos'.DIRECTORY_SEPARATOR.'portada_'.$curso->getId().'.jpg')) : ?>
                                        <td><?php echo sexy_button_to('Eliminar', 'admin/eliminarImagenCurso?idcurso='.$curso->getId(),'onclick= return confirm(\'¿Estas seguro que deseas eliminar la imagen del curso?\') '); ?></td>
-                                      <? endif; ?>
+                                      <?php endif; ?>
                                  </tr>
                                </table>
 
@@ -44,13 +44,13 @@
             </td>
           </tr>
       </table>
-      <br><? use_helper('volver');  echo volver(); ?>
-  <? else : ?>
+      <br><?php use_helper('volver');  echo volver(); ?>
+  <?php else : ?>
       <?php echo image_tag('ico_p_endok.gif'); ?> Guardado
       <?php use_helper('javascriptAjax') ?>
       <?php echo cargaPagina('admin/cursos') ?>
 
-  <? endif; ?>
+  <?php endif; ?>
 
     <br>
 
