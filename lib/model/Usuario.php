842c5fdffa58bec0f81f28a511704c88e9d0564c
mauro <mauro@icox.com.ar>
2018-01-19 15:50:14 +0000

    update email

--- lib/model/Usuario.php
+++ lib/model/Usuario.php
@@ -1042,7 +1042,7 @@ class Usuario extends BaseUsuario
     $mail->setPassword(sfConfig::get('app_empresa_pwdEmail'));
 
     //$mail->setSender($direccionde, $nombrede);
-    $mail->setFrom($direccionde, $nombrede);
+    $mail->setFrom('mauro@icox.com', $nombrede);
     $mail->addAddress($direccionpara, $nombrepara);
     $mail->setSubject($asunto);
     $mail->setContentType('text/html');
