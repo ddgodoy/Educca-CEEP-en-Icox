1d3ef51956e9b93b3f953e0bd4c69e57e2c842a7
mauro@icox.com.ar <mauro@icox.com.ar@668c4f68-67d6-4dea-8ad2-d905167ba806>
2018-01-22 17:27:50 +0000

    update evaluacion

--- trunk/apps/frontend/modules/tareas/templates/listarTareasSuccess.php
+++ trunk/apps/frontend/modules/tareas/templates/listarTareasSuccess.php
@@ -17,20 +17,21 @@
   <table class="tabla_cambiar_tareas">
     <?php $ahora = time();?>
     <?php for ($index = 0; $index < sizeof($eventos); $index++): ?>
-       <?php $fondo = (($index % 2 == 0))? " id=\"filarayada_verde\"" : ""; ?>
-       <?php echo("<tr$fondo>"); ?>
-        <?php $tarea = $tareas[$index];?>
-        <?php $evento = $eventos[$index];?>
-        <?php $curso = $cursos[$index];?>
-        <?php $tipo_evento = $tipos_evento[$index];?>
+       <?php if($tarea->getId() != 354): ?>
+            <?php $fondo = (($index % 2 == 0))? " id=\"filarayada_verde\"" : ""; ?>
+            <?php echo("<tr$fondo>"); ?>
+             <?php $tarea = $tareas[$index];?>
+             <?php $evento = $eventos[$index];?>
+             <?php $curso = $cursos[$index];?>
+             <?php $tipo_evento = $tipos_evento[$index];?>
 
-        <td class="td1"><?php echo link_to($evento->getDescripcion(), 'tareas/mostrarTarea?id_tarea='.$tarea->getId(),array('id'=>'ln_tarea_'.$tarea->getId())) ?></td>
-        <td class="td2"><?php echo $curso->getNombre() ?></td>
-        <td class="td3"><?php echo $tipo_evento->getDescripcion() ?></td>
-        <td class="td4"><?php echo $tarea->contar_alumnos() ?></td>
-        <td class="td5"><?php echo($evento->getFechaInicio('d-m-Y')); ?></td>
-        <td class="td6"><?php echo($evento->getFechaFin('d-m-Y')); ?></td>
-
+             <td class="td1"><?php echo link_to($evento->getDescripcion(), 'tareas/mostrarTarea?id_tarea='.$tarea->getId(),array('id'=>'ln_tarea_'.$tarea->getId())) ?></td>
+             <td class="td2"><?php echo $curso->getNombre() ?></td>
+             <td class="td3"><?php echo $tipo_evento->getDescripcion() ?></td>
+             <td class="td4"><?php echo $tarea->contar_alumnos() ?></td>
+             <td class="td5"><?php echo($evento->getFechaInicio('d-m-Y')); ?></td>
+             <td class="td6"><?php echo($evento->getFechaFin('d-m-Y')); ?></td>
+        <?php endif; ?>      
       </tr>
     <?php endfor; ?>
     </table>
