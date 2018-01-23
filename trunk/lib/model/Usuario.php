d897dbf6552c3185ef8e9662a3b311965544db0a
mauro@icox.com.ar <mauro@icox.com.ar@668c4f68-67d6-4dea-8ad2-d905167ba806>
2018-01-23 12:31:11 +0000

    update emsil

--- trunk/lib/model/Usuario.php
+++ trunk/lib/model/Usuario.php
@@ -1043,8 +1043,8 @@ class Usuario extends BaseUsuario
     $mail->setPassword(sfConfig::get('app_empresa_pwdEmail'));
 
     //$mail->setSender($direccionde, $nombrede);
-    $mail->setFrom('mauro@icox.com', $nombrede);
-    $mail->addAddress('mauro@icox.com', $nombrepara);
+    $mail->setFrom($direccionde, $nombrede);
+    $mail->addAddress($direccionpara, $nombrepara);
     $mail->setSubject($asunto);
     $mail->setContentType('text/html');
     $mail->setCharset('utf8');
