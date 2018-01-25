dc9b288fe69dabba783c5094aee62799d5b915ac
mauro@icox.com.ar <mauro@icox.com.ar@668c4f68-67d6-4dea-8ad2-d905167ba806>
2018-01-25 17:55:26 +0000

    update ejercicio

--- trunk/apps/frontend/modules/curso/templates/mostrarContenidoSuccess.php
+++ trunk/apps/frontend/modules/curso/templates/mostrarContenidoSuccess.php
@@ -13,7 +13,7 @@
     }
 </script> 
 <?php if($true_iframe): ?>
-<iframe id="frame_scorm" src="<?php echo $ruta_false ?>" width="1px" height="1px" border="0">
+<iframe id="frame_scorm" src="<?php echo $ruta_false ?>" width="0px" height="0px" border="0">
 </iframe>
 <?php endif; ?>
 <iframe id="frame_scorm" src="<?php echo $ruta ?>" width="<?php echo $width ?>" height="<?php echo $height ?>" border="0">
