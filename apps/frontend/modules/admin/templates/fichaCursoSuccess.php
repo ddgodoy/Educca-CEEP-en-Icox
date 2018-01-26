5e2df39532f9d0b68c317614f45bfb3f58d56f40
mauro <mauro@icox.com.ar>
2018-01-26 18:39:25 +0000

    update admin

--- apps/frontend/modules/admin/templates/fichaCursoSuccess.php
+++ apps/frontend/modules/admin/templates/fichaCursoSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('informacion'); ?>
-<? if (isset($info)) :  ?>
+<?php if (isset($info)) :  ?>
   <?php include_component('curso', 'fichaCurso',array('idcurso' => $idcurso,'info' => $info)) ?>
-<? else : ?>
+<?php else : ?>
   <?php include_component('curso', 'fichaCurso',array('idcurso' => $idcurso)) ?>
-<? endif ;?>
+<?php endif ;?>
 
