a1622125e62aa6aa063adc3a688e054e4d20496d
mauro@icox.com.ar <mauro@icox.com.ar@668c4f68-67d6-4dea-8ad2-d905167ba806>
2018-03-08 17:08:32 +0000

    update mensaje

--- trunk/apps/frontend/modules/mensaje/templates/redactarMensajeSuccess.php
+++ trunk/apps/frontend/modules/mensaje/templates/redactarMensajeSuccess.php
@@ -110,7 +110,7 @@ function addFile()
                                   <li><input type="file" name="upfile1" id="upfile1" class="file_input"></li>
                               </ul>
                           </td>
-                          <td><strong onclick="addFile()"><?php echo image_tag('add_icon.gif','Title=Agregar Archivo class=ico_profesor'); ?></strong></td> 
+                          <td><strong onclick="addFile()" style="cursor: pointer"><?php echo image_tag('add_icon.gif','Title=Agregar Archivo class=ico_profesor'); ?></strong></td> 
                         </tr>
                     </table>
                 </td>
