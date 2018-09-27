0c43b2cb3cede9368dde50efad236d7f1ac4a734
Robert <robert.mendoza8@gmail.com>
2018-09-27 01:17:30 -0400

    Resubiendo Web

---
+++ web/materias/27/shared/js/estilos.js
@@ -0,0 +1,93 @@
+// CHECK THE BROWSERS
+is=[];
+is.dom  = document.getElementById?1:0;
+is.ua   = navigator.userAgent.toLowerCase();
+is.ie   = is.ua.match(/msie/)||0;
+function isIE(){return (is.ie)?true:false;}
+function isDOM(){return (is.dom)?true:false;}
+is.estilo='<LINK rel="stylesheet" href="/css/curso'+(isIE()?"":"_netsc")+'.css" type="text/css">';
+document.write(is.estilo);
+pintado=true;
+
+/***********************************************
+* Cool DHTML tooltip script- ? Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+var offsetxpoint=-60 //Customize x offset of tooltip
+var offsetypoint=20 //Customize y offset of tooltip
+var ie=document.all
+var ns6=document.getElementById && !document.all
+var enabletip=false
+function ietruebody(){
+
+    return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+}
+
+function ddrivetip(thetext, thecolor, thewidth){
+    if (ns6||ie){
+        tipobj = document.getElementById("dhtmltooltip");
+        if (typeof thewidth!="undefined") tipobj.style.width=thewidth+"px"
+        if (typeof thecolor!="undefined" && thecolor!="") tipobj.style.backgroundColor=thecolor
+        tipobj.innerHTML=thetext
+        enabletip=true
+        return false
+    }
+}
+
+function ddrivetip2(thetext, thecolor, thewidth){
+    if (ns6||ie){
+        tipobj = document.getElementById("dhtmltooltip2");
+        if (typeof thewidth!="undefined") tipobj.style.width=thewidth+"px"
+        if (typeof thecolor!="undefined" && thecolor!="") tipobj.style.backgroundColor=thecolor
+        tipobj.innerHTML="<img src='../shared/imagenes/bombilla.gif' alt='tip' width='16' height='16' />"+thetext
+        enabletip=true
+        return false
+    }
+}
+
+function positiontip(e){
+    if (enabletip){
+        var curX=(ns6)?e.pageX : event.x+ietruebody().scrollLeft;
+        var curY=(ns6)?e.pageY : event.y+ietruebody().scrollTop;
+        //Find out how close the mouse is to the corner of the window
+        var rightedge=ie&&!window.opera? ietruebody().clientWidth-event.clientX-offsetxpoint : window.innerWidth-e.clientX-offsetxpoint-20
+        var bottomedge=ie&&!window.opera? ietruebody().clientHeight-event.clientY-offsetypoint : window.innerHeight-e.clientY-offsetypoint-20
+        var leftedge=(offsetxpoint<0)? offsetxpoint*(-1) : -1000
+        //if the horizontal distance isn't enough to accomodate the width of the context menu
+        if (rightedge<tipobj.offsetWidth)
+            //move the horizontal position of the menu to the left by it's width
+            tipobj.style.left=ie? ietruebody().scrollLeft+event.clientX-tipobj.offsetWidth+"px" : window.pageXOffset+e.clientX-tipobj.offsetWidth+"px"
+        else if (curX<leftedge)
+            tipobj.style.left="5px"
+        else
+            //position the horizontal position of the menu where the mouse is positioned
+            tipobj.style.left=curX+offsetxpoint+"px"
+        //same concept with the vertical position
+        if (bottomedge<tipobj.offsetHeight)
+            tipobj.style.top=ie? ietruebody().scrollTop+event.clientY-tipobj.offsetHeight-offsetypoint+"px" : window.pageYOffset+e.clientY-tipobj.offsetHeight-offsetypoint+"px"
+        else
+            tipobj.style.top=curY+offsetypoint+"px"
+        tipobj.style.visibility="visible"
+    }
+}
+
+function hideddrivetip(){
+    if (ns6||ie){
+        enabletip=false
+        tipobj.style.visibility="hidden"
+        tipobj.style.left="-1000px"
+        tipobj.style.backgroundColor=''
+        tipobj.style.width=''
+    }
+}
+
+document.onmousemove=positiontip
+<!--
+function popupcentree(page,largeur,hauteur,options) {
+    var top=(screen.height-hauteur)/2;
+    var left=(screen.width-largeur)/2;
+    window.open(page,"","top="+top+",left="+left+",width="+largeur+",height="+hauteur+","+options);
+}
+//-->
